ac0eaa9ea4d89eb5c79d9e7261913b037792871d
B-Borecki <bb_orecki@outlook.com>
2023-06-05 22:38:45 +0200

    Added sqlite database

---
+++ BgtMng.py
@@ -0,0 +1,32 @@
+class Budget:
+    acc_balance
+    history
+    prev
+    curr
+
+class Remuneration:
+    salary
+    def add_salary():
+        None
+    def modify_budget(budget):
+        None
+
+class Charges:
+    bills
+    def add_bill():
+        None
+    def modify_budget(budget):
+        None
+
+class Subscriptions:
+    subs
+    def add_sub():
+        None
+
+class Payments:
+    def reduce_budget(budget):
+        None
+
+class Recognitions:
+    def increase_budget(budget):
+        None

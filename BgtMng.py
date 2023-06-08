18c47ef521915f1f2e67261384dac1bcfbb1b39b
B-Borecki <bb_orecki@outlook.com>
2023-06-09 00:10:20 +0200

    Added method of recurring payments modifying the budget and adding them to the payment history

--- BgtMng.py
+++ BgtMng.py
@@ -7,22 +7,43 @@ class Budget:
     history = con.cursor().execute("""SELECT * FROM HISTORY;""").fetchall()
     prev = con.cursor().execute("SELECT date from DATES \
         WHERE name == 'previous_launch'").fetchall()[0][0]
-    prev = dt.datetime.strptime(prev, '%d.%m.%Y').date()
+    prev = dt.datetime.strptime(prev, '%d-%m-%Y').date()
     curr = dt.date.today()
 
 class Remuneration:
-    salary
-    def add_salary():
-        None
-    def modify_budget(budget):
-        None
+    salary = con.cursor().execute("""SELECT * FROM SALARY;""").fetchall()
+    def add_salary(this, name, amount, payday):
+        this.salary.append((name, amount, payday))
+    def modify_budget(this, budget):
+        for s in this.salary:
+            payday = s[2]
+            amount = s[1]
+            name = s[0]
+            add_day = dt.timedelta(days=1)
+            it = budget.prev + add_day
+            while it <= budget.curr:
+                if ((it+add_day).day == 1 and payday > it.day) or it.day == payday:
+                    budget.acc_balance += s[1]
+                    budget.history.append((name, amount, it.strftime("%Y-%m-%d"), budget.acc_balance))
+                it += add_day
+
 
 class Charges:
-    bills
-    def add_bill():
-        None
-    def modify_budget(budget):
-        None
+    bills = con.cursor().execute("""SELECT * FROM BILLS;""").fetchall()
+    def add_bill(this, name, amount, payday):
+        this.bills.append((name, amount, payday))
+    def modify_budget(this, budget):
+        for b in this.bills:
+            payment_day = s[2]
+            amount = s[1]
+            name = s[0]
+            add_day = dt.timedelta(days=1)
+            it = budget.prev + add_day
+            while it <= budget.curr:
+                if ((it+add_day).day == 1 and payment_day > it.day) or it.day == payment_day:
+                    budget.acc_balance += s[1]
+                    budget.history.append((name, amount, it.strftime("%Y-%m-%d"), budget.acc_balance))SSS
+                it += add_day
 
 class Subscriptions:
     subs
@@ -36,5 +57,3 @@ class Payments:
 class Recognitions:
     def increase_budget(budget):
         None
-
-con.close()
